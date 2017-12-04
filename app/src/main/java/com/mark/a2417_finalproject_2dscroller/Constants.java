7d1dab3cf1bbf83b21d3d552086f4c9a015dd787
MF <notsure@compuserv.com>
2017-12-03 19:39:43 -0600

    set screen to fullscreen and added Constants class to hold static values

---
+++ app/src/main/java/com/mark/a2417_finalproject_2dscroller/Constants.java
@@ -0,0 +1,11 @@
+package com.mark.a2417_finalproject_2dscroller;
+
+/**
+ * Class to hold static values to be used throughout game.
+ */
+
+public class Constants {
+
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+}
--- app/src/main/java/com/mark/a2417_finalproject_2dscroller/MainActivity.java
+++ app/src/main/java/com/mark/a2417_finalproject_2dscroller/MainActivity.java
@@ -1,13 +1,32 @@
 package com.mark.a2417_finalproject_2dscroller;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Make app fullscreen.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Get rid of title bar on top.
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
     }
 }
+
+
+
+
+
+
+
+
+
+// References:
+    // game setup and processes - https://www.youtube.com/watch?v=OojQitoAEXs
