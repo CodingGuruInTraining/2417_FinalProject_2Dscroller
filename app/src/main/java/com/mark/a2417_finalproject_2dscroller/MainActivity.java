a9cd6a4eebdf83d0494e28db3a361a2bb07f516d
MF <notsure@compuserv.com>
2017-12-03 19:31:47 -0600

    initial commit

---
+++ app/src/main/java/com/mark/a2417_finalproject_2dscroller/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mark.a2417_finalproject_2dscroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
